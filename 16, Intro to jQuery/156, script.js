41c73121c27128befc64e3ab9939c637658b5ce7
Mikael Wallin <mikael.wallin@gmail.com>
2016-07-16 11:44:50 +0200

    Added typings for jQuery and som intro exercises

---
+++ 16, Intro to jQuery/156, script.js
@@ -0,0 +1,23 @@
+/// <reference path="../typings/main.d.ts" />
+
+/* global $:false, jQuery:false */
+
+// Check to see if jQuery is loaded OK
+if (jQuery) {
+  alert("jQuery loaded!");
+} else {
+  alert("jQuery not loaded :(!");
+}
+
+
+// Select all divs and give them a purple background
+$("div").css("background", "purple");   
+
+// Select the divs with class highlight and make them 200px wide
+$(".highlight").css("width", "200px");
+
+// Select the div with id "third"" and give it an orange border
+$("#third").css("border", "5px solid orange");
+
+// Select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
---
+++ 16, Intro to jQuery/157, script.js
@@ -0,0 +1,5 @@
+/// <reference path="../typings/main.d.ts" />
+
+/* global $:false, jQuery:false */
+
+
