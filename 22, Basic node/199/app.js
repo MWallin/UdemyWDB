65c0a54d8a773950e025df7c86413bc308a06e87
Mikael Wallin <Mikael Wallin>
2016-07-19 18:38:45 +0200

    Made an inventory list

---
+++ 22, Basic node/199/app.js
@@ -0,0 +1,29 @@
+var faker = require("faker");
+
+
+function makePricedProducts() {
+  
+  return "Product: " + faker.commerce.productName() + " - Price: $" + faker.commerce.price(); 
+
+}
+
+
+function printInventoryList() {
+  
+  var items = 10;
+
+  console.log("###################################################");
+  console.log("Inventory list");
+  console.log("###################################################");
+
+  for (var index = 0; index < items; index++) {
+    
+    console.log(makePricedProducts());
+    
+  }
+
+}
+
+
+// Print out the inventory list
+printInventoryList();
