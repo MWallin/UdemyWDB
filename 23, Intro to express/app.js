a4a48a3e974859ac6da0df8288bd1a98504b7f2f
Mikael Wallin <Mikael Wallin>
2016-07-20 17:33:54 +0200

    Started intro to express plus additional typings

---
+++ 23, Intro to express/app.js
@@ -0,0 +1,57 @@
+/// <reference path="../typings/index.d.ts" />
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+// App setup
+
+var express = require("express");
+var app = express();
+
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+
+
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+// Routes
+
+// "/" => Hi
+app.get("/", function (req, res){
+
+  res.send("Hi there!");
+
+});
+
+
+// "/bye" => Goodbye
+app.get("/bye", function (req, res) {
+
+  res.send("Bye bye...");
+
+});
+
+
+// "/dog" => Meow!!
+app.get("/dog", function (req, res) {
+  
+  res.send("Meow!");
+
+});
+
+
+
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+
+
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+// Server start
+
+app.listen(3000, function () {
+  console.log("Server has started!");
+});
+
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
